b38ade44a012840f74ad7562025335857aab21b6
Christopher Gronewold <chrisrgronewold@gmail.com>
2023-04-15 17:52:31 -0500

    finished core typing functionality

--- components/typingComponent.py
+++ components/typingComponent.py
@@ -1,3 +1,4 @@
+import random
 import threading
 import tkinter
 import tkinter.messagebox
@@ -12,35 +13,66 @@ class TypingComponent(customtkinter.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
 
+        self.typed = ""
+        self.sentence = ""
+
         self.grid(row=0, column=1, padx=(5, 0), pady=(5, 0), rowspan=4, sticky="nsew")
 
-        """self.playing = False
-        self.start_typing_button = customtkinter.CTkButton(master=self, text="Start", fg_color="transparent",
-                                                           border_width=2, text_color=("gray10", "#DCE4EE"))
-        self.start_typing_button.grid(row=2, column=0)
-        self.start_typing_button.place(relx=0.4, rely=0.6)"""
-        #self.start_typing()
-        #root = tkinter.Tk()
+        self.reset()
 
-        # bind the on_key_press function to the <Key> event
-        #self.start_typing_button.bind('<Key>', self.on_key_press)
+
+    def reset(self):
+        self.typed = ""
+
+        f = open('components/10k_words.txt').read()
+        sentences = f.split('\n')
+        words = []
+        for _ in range(5):
+            words.append(random.choice(sentences))
+        self.sentence = " ".join(words)
+        print(self.sentence)
+        self.sentence_label = customtkinter.CTkLabel(master=self, text=self.sentence, anchor="w",
+                                                     font=customtkinter.CTkFont(size=30, weight="bold"), text_color=("gray"))
+        self.sentence_label.grid(row=2, column=0)
+        self.sentence_label.place(relx=0.1, rely=0.5)
+
         input_queue = queue.Queue()
-        kb_input_thread = threading.Thread(target=self.check_esc_pressed, args=(input_queue,))
+        kb_input_thread = threading.Thread(target=self.check_letter_pressed, args=(input_queue,))
         kb_input_thread.daemon = True
         kb_input_thread.start()
 
-    def check_esc_pressed(self, input_queue):
+    def check_letter_pressed(self, input_queue):
         while True:
             event = keyboard.read_event()
             if event.event_type == 'down' and event.name.isalpha():
-                print(event.name)
-            #time.sleep(0.1)  # seconds
-
-    """def on_press(self, event):
-        if event.event_type == 'down' and event.name.isalpha():
-            print(event.name)
+                print("Typing Begins")
+                self.typed += event.name
+                self.start_typing()
+                break
 
     def start_typing(self):
-        keyboard.on_press(self.on_press)
-        keyboard.wait()
-        #self.start_typing_button.destroy()"""
+        while self.typed != self.sentence:
+            if self.typed == self.sentence[:len(self.typed)]:
+                self.typed_label = customtkinter.CTkLabel(master=self, text=self.typed, anchor="w",
+                                                         font=customtkinter.CTkFont(size=30, weight="bold"), text_color=("white"))
+            else:
+                self.typed_label = customtkinter.CTkLabel(master=self, text=self.typed, anchor="w",
+                                                         font=customtkinter.CTkFont(size=30, weight="bold"), text_color=("red"))
+            self.typed_label.grid(row=2, column=0)
+            self.typed_label.place(relx=0.1, rely=0.5)
+            event = keyboard.read_event()
+            if event.event_type == 'down':
+                if event.name == 'backspace':
+                    self.typed = self.typed[:-1]
+                else:
+                    if event.name == 'space':
+                        event.name = ' '
+                    elif event.name == 'period':
+                        event.name = '.'
+                    self.typed += event.name
+                    print(self.typed)
+            self.typed_label.destroy()
+
+        self.sentence_label.destroy()
+        self.reset()
+
