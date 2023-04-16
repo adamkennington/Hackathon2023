1e77cfa0efbdec6054d5364cb49e3cc75ca492e7
EthanTB <ethantberkley@gmail.com>
2023-04-15 21:02:43 -0500

    Shower Problems corrected...

--- components/showerComponent.py
+++ components/showerComponent.py
@@ -1,16 +1,18 @@
 import tkinter
 import tkinter.messagebox
 import customtkinter
+from random import randrange, random
 
 class ShowerComponent(customtkinter.CTkFrame):
-    def __init__(self, master, **kwargs):
+    def __init__(self, master, parentAfter, **kwargs):
         super().__init__(master, **kwargs)
+        self.parentAfter = parentAfter
         self.bestTime = 0
         self.yourTime = 0
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_rowconfigure(1, weight=3)
-        self.grid_rowconfigure(2, weight=1)
-        self.grid_rowconfigure(3, weight=1)
+        self.grid_rowconfigure((0, 1, 2, 3, 4), weight=1)
+        # self.grid_rowconfigure(1, weight=1)
+        # self.grid_rowconfigure(2, weight=1)
+        # self.grid_rowconfigure(3, weight=1)
 
         self.grid_columnconfigure((0, 1, 2), weight=1)
         
@@ -18,16 +20,19 @@ class ShowerComponent(customtkinter.CTkFrame):
 
         self.yourTimeDisplay = customtkinter.CTkLabel(self, text=f"It has been {self.yourTime} days since you have taken a shower.")
 
-        self.prompt =  customtkinter.CTkLabel(self, text=f"Type how long it has been since you have showered below!")
+        self.prompt =  customtkinter.CTkLabel(self, text=f"Press the button to have your shower delay calculated!")
         
-        self.bestTimeDisplay.grid(row=1, column=1, sticky="nsew")
+        self.calculatebtn = customtkinter.CTkButton(self, text="Press Me!", command=self.calculate)
 
-        self.yourTimeDisplay.grid(row=2, column=1, sticky="nsew")
+        self.bestTimeDisplay.grid(row=0, column=1, sticky="nsew")
 
-        self.prompt.grid(row=3, column=1, sticky="nsew")
+        self.yourTimeDisplay.grid(row=1, column=1, sticky="nsew")
 
-    def entry_event(self, text):
-        self.yourTime = int(text)
+        self.prompt.grid(row=2, column=1, sticky="nsew")
+        self.calculatebtn.grid(row=3, column=1, sticky="nsew")
+
+    def entry_event(self, shower_variable):
+        self.yourTime = shower_variable
         if self.yourTime > self.bestTime:
             self.yourTimeDisplay.configure(text=f"WOW! It has been more than {self.bestTime} days since you have taken a shower?!?")
             self.bestTime = self.yourTime
@@ -36,5 +41,10 @@ class ShowerComponent(customtkinter.CTkFrame):
             self.yourTimeDisplay.configure(text=f"It has been {self.yourTime} days since you have taken a shower.")
 
 
+    def calculate(self):
+        self.calculatebtn.configure(text="Sniffing...", state="disabled")
+        self.parentAfter(500 + int(random()*3000), self.afterDelay)
 
-
+    def afterDelay(self):
+        self.calculatebtn.configure(text="Press Me!", state="normal")
+        self.entry_event(randrange(0, 25))
